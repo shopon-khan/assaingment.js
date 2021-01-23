002a8ace1da58178b1ec7cf65ed48e173d2b2cfe
SHOPON <shoponkhan212020@gmail.com>
2021-01-23 17:52:43 +0600

    adda all file

---
+++ assaingment.js
@@ -0,0 +1,32 @@
+
+// kilometerToMeter
+function kilometerToMeter(kilo){
+    var met = kilo * 1000
+    return met;
+}
+var nanarMet = kilometerToMeter (10);
+console.log(nanarMet);
+var naniMet = kilometerToMeter (15);
+console.log(naniMet);
+
+// bugetCalculator
+function budgetCalculator (watchPrice, mobilePrice, laptopPrice){
+    var result = watchPrice + mobilePrice + laptopPrice;
+    return result;
+}
+var sum = budgetCalculator (50, 100, 500);
+console.log (sum);
+
+
+//megaFriend
+
+
+var friend = [ "amir", "samir", "mahamud", "rayhan", "mehedi", "shoponkhan"];
+  var max = friend [0];
+ for ( var i= 0; i < friend.length; i++){
+      var element = friend[i];
+     if ( element> max){
+          max = element;
+      }
+ }
+  console.log("the fine name is:", max);
